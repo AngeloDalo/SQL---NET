20d3c7812ed735503608e9d270aa952e008a4331
AngeloDalo <angelo251158@gmail.com>
2022-05-08 14:44:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseCors(options =>
+    options 
+        .WithOrigins("http://localhost:4200")
+        .WithMethods("POST", "PUT", "DELATE", "GET")
+        .AllowAnyHeader()
+);
+
+app.MapControllers();
+
+app.Run();
+ 
